4d09a6460d0581761aa1789dad6e1af8e3e9c883
Cristhian Villegas <cristianvg9692@gmail.com>
2019-12-28 00:36:13 -0600

    Subiendo inviteichon

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -9,12 +9,12 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
-	public listaPublicaciones: any;
+	public listaPublicaciones: any = [];
 
   constructor(private post: HttpClient) {}
 
   ngOnInit() {
-  	this.obtenerPublicaciones();
+    this.obtenerPublicaciones();
   }
 
   /** MÉTODOS DE PETICIÓN
@@ -24,17 +24,41 @@ export class PerfilComponent implements OnInit {
   * DELETE
   */
   public obtenerPublicaciones(): void {
-  	this.post.get('https://jsonplaceholder.typicode.com/posts').subscribe(
+  	/** this.post.get('https://jsonplaceholder.typicode.com/posts').subscribe(
   		response => {
   			console.log(response);
   			this.listaPublicaciones = response;
   		}, 
   		error => {}
-  	);
+  	);*/
+    console.log(localStorage.getItem('post'));
+    this.listaPublicaciones = (JSON.parse(localStorage.getItem('post')) == null) ? [] : JSON.parse(localStorage.getItem('post'));
   }
 
   public darLike(publicacion: string): void {
   	alert('Le has dado like a ' + publicacion);
+  }
+
+  public publicarPost(post: string): void {
+    if(post !== '' && post !== undefined && post != "") {
+
+      this.listaPublicaciones = (JSON.parse(localStorage.getItem('post')) == null) ? [] : JSON.parse(localStorage.getItem('post'));
+      console.log(this.listaPublicaciones);
+
+      var amorcito = 4 + 5;
+
+      var publicacion = {
+          title: amorcito,
+          body: post
+       };
+
+       this.listaPublicaciones.push(publicacion);
+      
+      // Para almacenar datos en el navegador se utiliza localstorage
+      localStorage.setItem('post', JSON.stringify(this.listaPublicaciones));
+      
+      this.obtenerPublicaciones();
+    }
   }
 
 }
